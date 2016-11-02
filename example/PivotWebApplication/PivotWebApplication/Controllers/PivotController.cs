cf2cea38e2e82577b29268a23857e3a83665ba42
Flexmonster <github@flexmonster.com>
2016-11-02 20:12:27 +0200

    Add MVC example

---
+++ example/PivotWebApplication/PivotWebApplication/Controllers/PivotController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PivotWebApplication.Controllers
+{
+    public class PivotController : Controller
+    {
+        // GET: Pivot
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
